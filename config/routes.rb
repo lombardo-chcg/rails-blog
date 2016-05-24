33818caf344d99dbd0f27b87db76e9df8611b382
Dominick Lombardo <email": null>
2016-05-24 18:43:41 -0500

    Removed resources method from routes.rb and hand-coded all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
-  resources :articles
+  # resources :articles
 
-  resources :articles do
-    resources :comments
-  end
+  get   '/articles'           => 'articles#index'
+  post  '/articles'           => 'articles#create'
+  get   '/articles/new'       => 'articles#new', as: :new_article
+  get   '/articles/:id/edit'  => 'articles#edit', as: :edit_article
+  get   '/articles/:id'       => 'articles#show', as: :article
+  patch '/articles/:id'       => 'articles#update'
+  delete '/articles/:id'      => 'articles#destroy'
+
+  get 'articles/:id/comments' => 'comments#index', as: :article_comments
+  post '/articles/:article_id/comments'    => 'comments#create'
+  get '/articles/:article_id/comments/new' => 'comments#new', as: :new_article_comment
+  get '/articles/:article_id/comments/:id' => 'comments#show', as: :article_comment
+  delete '/articles/:article_id/comments/:id' => 'comments#destroy'
+
+  # resources :articles do
+  #   resources :comments
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
