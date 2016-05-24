b2a56e7ec3a030f656f920d5bdc50362324e12d6
Dominick Lombardo <email": null>
2016-05-23 20:24:38 -0500

    Add ajax to comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,18 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment = @article.comments.new(comment_params)
+
+    respond_to do |format|
+        if @comment.save
+          format.html { redirect_to @comment.post }
+          format.js
+          format.json { render json: @user, status: :created, location: @user }
+        else
+          format.html { render :new }
+          format.json { render json: @comment.errors, status: :unprocessable_entity }
+        end
+    end
   end
 
   def destroy
