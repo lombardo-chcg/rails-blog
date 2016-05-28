ce4f06ad1313e70bab2b504336260c0d58d99df2
Dominick Lombardo <email": null>
2016-05-28 13:27:53 -0500

    Add virtual attributes to article class

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,14 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+
+  def author_full_name
+    [author_first_name, author_last_name].join(' ')
+  end
+
+  def author_full_name=(name)
+    name_array = name.split
+    self.author_first_name = name_array[0]
+    self.author_last_name = name_array[1]
+  end
 end
--- db/migrate/20160521220800_create_articles.rb
+++ db/migrate/20160521220800_create_articles.rb
@@ -3,6 +3,8 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :text
+      t.string :author_first_name
+      t.string  :author_last_name
 
       t.timestamps null: false
     end
